712a8367852c195510d4fcdbddcc5ba66fd7f2da
Tikiid <thibo92s@gmail.com>
2024-06-23 20:01:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -17,15 +17,14 @@ def create_secret_combination():
 def enter_combination():
     while True: 
         guess_combination = str(input('enter a combination (exemple: RYBG): ')).upper()
-    # print(type(guess_combination))
+
         if(check_guess_combination(guess_combination)):
             break
     
     return guess_combination
 
 
-def check_guess_combination(guess_combination):
-    
+def check_guess_combination(guess_combination):    
     if len(guess_combination) < 4:
         print("too short")
         return False
@@ -37,29 +36,29 @@ def check_guess_combination(guess_combination):
         if element not in list_of_colors:
             print("wrong caractere")
             return False
+
     return True
 
 
 def check_right_place(caractere, index, secret_combination):
     if caractere == secret_combination[index]:
         return True
+
     return False
 
 
 def check_existing_color(guess, secret_combination):
     exist_list = []
     for index, caractere in enumerate(guess) :
-        # print(f"caractere n°{index+1}")
         if caractere in secret_combination:
             if check_right_place(caractere, index, secret_combination): 
                 exist_list.append('!')
-                # print("color is in right place secret combination")
             else:
                 exist_list.append('?')
-                # print("color exist in secret combination")
+
         else: 
             exist_list.append('-')
-            # print("color do not exist in secret combination")
+
     join_exist_list = ''.join(exist_list) 
     return join_exist_list
 
@@ -89,34 +88,44 @@ def restart_game():
             print("Wrong input (Y for yes or N for no)")
     print('Too many try')
 
+
+# --- Display the rules --- #
+
 def start_messages():
     print('Start a game')
     print()
     print('Find the secret combination : R (red), B (blue), O (orange), G (green), Y (yellow), P (purple)')
     print('Hints : - (not in the secret combination), ? (wrong place), ! (right place)')
     print()
-    start_the_game = input("Press enter to start the game")
+    start_the_game = input("Press enter to start the game ")
     os.system('cls')
 
 restart = False
 
+# --- The game --- #
+
 while True:
-    # clear console
+
+    # --- Clear console --- # 
+
     os.system('cls')
 
-    # variables reset
+    # --- Variables reset --- #
+    
     attempts = 1
     secret_combination = ''
     guess = ''
     all_attemps_list = [[], [], [], [], [], [], [], [], [], []]
 
 
-    # create the secret combination
+    # --- Create the secret combination --- #
+
     secret_combination = create_secret_combination()
-    # print(secret_combination)
+    
     if not restart:
         start_messages()
     
+    # --- Current game --- #
 
     while attempts < 11:
         
@@ -124,18 +133,24 @@ while True:
             print('Start a new game')
             print()
             restart = False
-        
+
         display_board(all_attemps_list)
+
+        # --- Check if the player won --- #
 
         if guess == secret_combination:
             print('you win')
             break
+        
+        # --- Display information and the input to the player --- #
 
         print(f'attempts left : {11 - attempts}')
         guess = enter_combination()
         exist = check_existing_color(guess, secret_combination)
         all_attemps_list[10-attempts] = [guess, exist]
+        
         os.system('cls')
+        
         attempts += 1
     
     os.system('cls')
@@ -145,6 +160,9 @@ while True:
         print('you loose')
     
     print(f'The secret code was : {secret_combination}')
+
+    # --- Restart a game --- #
+
     restart = restart_game()
     if not restart:
         break
